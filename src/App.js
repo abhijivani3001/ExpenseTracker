c6de988849d38b0bba4f2633af4f3d58a83b76d9
abhijivani3001 <abhijivani3001@gmail.com>
2023-08-11 23:34:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const expenses = [
+    { id: 'e1', title: "Car Insurance", amount: 259.5, date: new Date(2023, 4, 24) },
+    { id: 'e2', title: "Books", amount: 140.7, date: new Date(2022, 1, 9) },
+    { id: 'e3', title: "Food", amount: 29.5, date: new Date(2023, 9, 14) },
+    { id: 'e4', title: "Clothes", amount: 129.5, date: new Date(2023, 9, 14) },
+  ];
+
+  // return (
+  //   <div>
+  //     <div className="First">Hello</div>
+  //     <Expenses item={expenses}/>
+  //   </div>
+  // );
+
+  // Alternative return code of above code in JSX:
+  // Syntax: return React.createElement(element_tag, {Object_that_specifies_the_attributes}, content_inside_tag);
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('div',{className: 'First'},'Hello'),
+    React.createElement(Expenses,{item: expenses})
   );
 }
 
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.item[0].title}
+        amt={props.item[0].amount}
+        date={props.item[0].date}
+      />
+      <ExpenseItem
+        title={props.item[1].title}
+        amt={props.item[1].amount}
+        date={props.item[1].date}
+      />
+      <ExpenseItem
+        title={props.item[2].title}
+        amt={props.item[2].amount}
+        date={props.item[2].date}
+      />
+      <ExpenseItem
+        title={props.item[3].title}
+        amt={props.item[3].amount}
+        date={props.item[3].date}
+      />
+    </Card>
+  );
+};
+export default Expenses;
