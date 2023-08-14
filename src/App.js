6fdc9faa1ba11787aa0041547a84f413a3c3b561
abhijivani3001 <abhijivani3001@gmail.com>
2023-08-14 07:15:33 +0530

    added conditionals in filtered by year

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const DUMMY_EXPENSES = [
   { id: 'e1', title: "Car Insurance", amount: 259.5, date: new Date(2023, 4, 24) },
   { id: 'e2', title: "Books", amount: 140.7, date: new Date(2022, 1, 9) },
   { id: 'e3', title: "Food", amount: 29.5, date: new Date(2023, 9, 14) },
-  { id: 'e4', title: "Clothes", amount: 129.5, date: new Date(2023, 9, 14) },
+  { id: 'e4', title: "Clothes", amount: 129.5, date: new Date(2019, 9, 14) },
 ];
 
 const App = () => {
